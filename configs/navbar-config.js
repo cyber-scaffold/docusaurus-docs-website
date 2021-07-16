3e68d28606e7b369b9be27fb56c28b5fbc64a2b4
yushicheng <1542874601@qq.com>
2021-07-16 18:26:37 +0800

    init:项目初始化 星期五 晚上 6:26:36 七月 16日 2021 项目初始化提交

---
+++ configs/navbar-config.js
@@ -0,0 +1,20 @@
+
+
+module.exports={
+  title: "HOMEPAGE-首页",
+  // logo: {
+  //   alt: "My Site Logo",
+  //   src: "img/logo.svg",
+  // },
+  items: [{
+    type: "doc",
+    docId: "intro",
+    position: "right",
+    label: "项目简介",
+  },{
+    type: "doc",
+    docId: "test-demo/test-demo-main",
+    position: "right",
+    label: "项目文档",
+  }]
+};
